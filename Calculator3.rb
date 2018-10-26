a4195119f9592940f59fa1da7f057b120d87a9d9
MarHtaHT <44250780+MarHtaHT@users.noreply.github.com>
2018-10-26 20:19:32 +0630

    Update Calculator3.rb

--- Calculator3.rb
+++ Calculator3.rb
@@ -1,46 +1,28 @@
-puts"Enter First Number"
-first=gets.chomp.to_i
-puts"Enter Secone Number"
-second=gets.chomp.to_i
-puts"Enter Operator(+,_,*,/)"
-operator=gets.chomp
-if operator=="+"
-print"#{first}+#{second}="
-puts first+second
-elsif operator=="-"
-print"#{first}-#{second}="
-puts first-second
-elsif operator=="*"
-print"#{first}*#{second}="
-puts first*second
-else operator=="/"
-print"#{first}/#{second}="
-puts first/second
-end
+begin
 loop do
-puts "Do you want to do another(y/n)"
-i=gets.chomp
-if i=="y"
-puts "Enter First Number"
-first =gets.chomp.to_i
-puts "Enter Second Number"
-second =gets.chomp.to_i
-puts "Enter opeator(+,-,*,/)"
-operator=gets.chomp
-if operator=="+"
-print"#{first}+#{second}="
+puts "Enter First Number:"
+first=gets.chomp.to_i
+puts "Enter Second Number:"
+second=gets.chomp.to_i
+puts "Enter Operator(+,-,*,/)"
+op=gets.chomp
+if op=="+"
+print "#{first}+#{second}="
 puts first+second
-elsif operator=="-"
-print"#{first}-#{second}="
+elsif op=="-"
+print "#{first}-#{second}="
 puts first-second
-elsif operator=="*"
-print"#{first}*#{second}="
+elsif op=="*"
+print "#{first}*#{second}="
 puts first*second
-else operator=="/"
-print"#{first}/#{second}="
+elsif op=="/"
+print "#{first}/#{second}="
 puts first/second
 end
-else
-break
+puts"Do you want to another(y/n)?"
+i=gets.chomp
+break if i=="n"
+rescue Exception=>e
+puts e.message+"is error."
 end
 end
